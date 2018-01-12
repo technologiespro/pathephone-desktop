8736528a6b19682f8fb3d1417dfbd426a5dad9bc
Максим Ильин <negamaxi@gmail.com>
2018-01-12 16:19:50 +0300

    chore

--- src/app/view/InitApp.js
+++ src/app/view/InitApp.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import Async from './_/Async.js'
 import initDb from '../scripts/initDb'
-import initIpfs from '../scripts/initIpfs'
+import initIpfs from '../scripts/openAlbumsGate'
 
 import App from './App'
 
