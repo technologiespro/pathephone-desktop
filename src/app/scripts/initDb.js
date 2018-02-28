09ed3728a35724f5758a7c4bd51361659c92b6d2
Максим Ильин <negamaxi@gmail.com>
2018-02-28 13:11:34 +0300

    initial

--- src/app/scripts/initDb.js
+++ src/app/scripts/initDb.js
@@ -1,21 +1,8 @@
+import PouchDB from 'pouchdb'
 import albums from '../data/albums'
-import getDb, { createDb } from '../api/rxdb'
 
-const initDb = async () => {
-  await createDb({
-    name: 'pathephone',
-    adapter: 'idb'
-  })
-  const db = getDb()
-  albums.collection = await db.collection({
-    name: 'albums',
-    schema: albums.rxdbSchema,
-    migrationStrategies: {
-      1: function (oldDoc) {
-        return null
-      }
-    }
-  })
+const initDb = () => {
+  albums.collection = new PouchDB('albums')
 }
 
 export default initDb
